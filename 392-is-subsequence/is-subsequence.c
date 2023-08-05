4b1c5fba9d59f277f128289f46e4c93919e0743c
ALBINI-AI <106315587+ALBINI-AI@users.noreply.github.com>
2023-08-06 01:55:58 +0530

    Time: 2 ms (57.89%) | Memory: 5.4 MB (86.96%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.c
@@ -0,0 +1,15 @@
+bool isSubsequence(char * s, char * t){
+    if(strlen(s)>strlen(t)) return false;
+    int len=strlen(s);
+    int count=0;
+    int j=0;
+    for(int i=0;s[i]!='\0';i++)
+        for(;t[j]!='\0';j++)
+            if(s[i]==t[j]){
+                count++;
+                j++;
+                break;}
+ 
+    if(len!=count) return false;
+    return true;
+}
