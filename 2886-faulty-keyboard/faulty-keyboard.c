874f8f4eb4e4a60e7249efa8c3c583d72f38b782
ALBINI-AI <106315587+ALBINI-AI@users.noreply.github.com>
2023-08-20 20:12:21 +0530

    Time: 4 ms (57.35%) | Memory: 5.8 MB (82.35%) - LeetSync

---
+++ 2886-faulty-keyboard/faulty-keyboard.c
@@ -0,0 +1,20 @@
+char * finalString(char * s){
+    int index=0;
+    int j;
+    int len=strlen(s);
+
+    for(int i=0;s[i]!='\0';i++)
+        if(s[i]=='i'){
+            for( j=i;j<len;j++)
+                s[j]=s[j+1];
+            len--;
+            int temp;
+            for(int k=0,j=i-1;k<j;k++,j--){
+                temp=s[k];
+                s[k]=s[j];
+                s[j]=temp;
+            }
+            i--;
+        }
+    return s;
+}
