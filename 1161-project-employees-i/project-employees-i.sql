0ce44c9d89865e00aa469f9cf5875e59d995efa4
ALBINI-AI <106315587+ALBINI-AI@users.noreply.github.com>
2023-09-30 15:03:27 +0530

    Time: 856 ms (97.25%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1161-project-employees-i/project-employees-i.sql
@@ -0,0 +1,4 @@
+SELECT Project.project_id, ROUND( AVG(Employee.experience_years),2 ) as average_years 
+FROM Project
+INNER JOIN Employee ON Project.employee_id = Employee.employee_id
+GROUP BY project_id
