583c3d7d79b21f5cb4abbabc5ffe2a88d9f7f611
ALBINI-AI <106315587+ALBINI-AI@users.noreply.github.com>
2024-01-05 02:56:18 +0530

    Time: 209 ms (90.91%) | Memory: 15.8 MB (45.45%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.c
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.c
@@ -2,7 +2,6 @@ int minOperations(int* nums, int numsSize){
     int freq[1000001]={0};
     int max=0;
     int ans=0;
-    int two=0;
     int three=0;
     for(int i=0;i<numsSize;i++){
         if(nums[i]>max)
@@ -12,7 +11,7 @@ int minOperations(int* nums, int numsSize){
     for(int i=0;i<=max;i++){
         three = freq[i]-freq[i]%3;
         while (three>0){
-            if(three%3==0 && (freq[i]-three)%2==0){
+            if((freq[i]-three)%2==0){
                 ans+=(three/3)+(freq[i]-three)/2;
                 break;
             }
